4c2f9ba43f7e89d39e42c519b31ab86346d389e6
kulsoomrasheed <rasheedamaan111@gmail.com>
2024-02-28 01:03:52 +0530

    changed

--- routes.js
+++ routes.js
@@ -8,8 +8,7 @@ btcRouter.get("/",async(req,res)=>{
         const btc = await BTC.find();
         res.status(200).json({msg:"Success",btc});
       } catch (error) {
-        console.error('Error fetching bitcoins:', error);
-        res.status(500).json({ error: 'An error occurred while fetching bitcoins' });
+        res.status(500).json({ error: 'An error occurred while fetching bitcoins' ,error});
       }
 })
 
